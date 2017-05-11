e24ec5991559da5fdf4816e1bcc1410661fbcd24
Unknown <matheus_felix@hotmail.com>
2017-05-10 23:45:04 -0300

    Trabalho utilizando windows forms e C#, ainda em desenvolvimento na faculdade

---
+++ Topicos Especiais II - C#/CorretagemDeImoveis/CorretagemDeImoveis/frmLogin.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Modelo;
+using Controle;
+
+namespace CorretagemDeImoveis
+{
+    public partial class frmLogin : Form
+    {
+        Usuario usuario;
+
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                usuario = new Usuario();
+                usuario.User = txbUsuario.Text;
+                usuario.Password = txbSenha.Text;
+                
+
+                if (UsuAutenticacao.ValidarUsuario(usuario))
+                {
+                    
+                    this.Tag = usuario;
+                    this.DialogResult = DialogResult.Yes;
+                    this.Close();
+                    
+                }
+                else
+                {
+                    lblMsgErro.Text = "Usuario ou senha incorretos!";
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro: " + ex.Message);
+            }
+
+        }
+
+        private void LimparMsgErro()
+        {
+            lblMsgErro.Text = "";
+        }
+
+        private void txb_Changed(object sender, EventArgs e)
+        {
+            LimparMsgErro();
+        }
+
+        public Usuario getUsuario()
+        {
+            return usuario;
+        }
+
+        private void frmLogin_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
