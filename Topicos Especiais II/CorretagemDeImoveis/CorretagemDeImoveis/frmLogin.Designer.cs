d218e5cc2cc60c43c546fa2d9653c72bbd294770
Unknown <matheus_felix@hotmail.com>
2017-05-10 23:40:59 -0300

    Add Topicos II melhor org

---
+++ Topicos Especiais II/CorretagemDeImoveis/CorretagemDeImoveis/frmLogin.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace CorretagemDeImoveis
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnLogar = new System.Windows.Forms.Button();
+            this.txbSenha = new System.Windows.Forms.TextBox();
+            this.txbUsuario = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblMsgErro = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancelar.Location = new System.Drawing.Point(164, 132);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(75, 23);
+            this.btnCancelar.TabIndex = 11;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // btnLogar
+            // 
+            this.btnLogar.Location = new System.Drawing.Point(47, 132);
+            this.btnLogar.Name = "btnLogar";
+            this.btnLogar.Size = new System.Drawing.Size(75, 23);
+            this.btnLogar.TabIndex = 10;
+            this.btnLogar.Text = "Logar";
+            this.btnLogar.UseVisualStyleBackColor = true;
+            this.btnLogar.Click += new System.EventHandler(this.btnLogar_Click);
+            // 
+            // txbSenha
+            // 
+            this.txbSenha.Location = new System.Drawing.Point(81, 75);
+            this.txbSenha.Name = "txbSenha";
+            this.txbSenha.Size = new System.Drawing.Size(191, 20);
+            this.txbSenha.TabIndex = 9;
+            this.txbSenha.Text = "123";
+            // 
+            // txbUsuario
+            // 
+            this.txbUsuario.Location = new System.Drawing.Point(81, 25);
+            this.txbUsuario.Name = "txbUsuario";
+            this.txbUsuario.Size = new System.Drawing.Size(191, 20);
+            this.txbUsuario.TabIndex = 8;
+            this.txbUsuario.Text = "Mat";
+            this.txbUsuario.TextChanged += new System.EventHandler(this.txb_Changed);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Senha:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Usuario:";
+            // 
+            // lblMsgErro
+            // 
+            this.lblMsgErro.AutoSize = true;
+            this.lblMsgErro.Location = new System.Drawing.Point(12, 183);
+            this.lblMsgErro.Name = "lblMsgErro";
+            this.lblMsgErro.Size = new System.Drawing.Size(16, 13);
+            this.lblMsgErro.TabIndex = 12;
+            this.lblMsgErro.Text = "...";
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 207);
+            this.Controls.Add(this.lblMsgErro);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnLogar);
+            this.Controls.Add(this.txbSenha);
+            this.Controls.Add(this.txbUsuario);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Name = "frmLogin";
+            this.Text = "Login Sistema";
+            this.Load += new System.EventHandler(this.frmLogin_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnLogar;
+        private System.Windows.Forms.TextBox txbSenha;
+        private System.Windows.Forms.TextBox txbUsuario;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblMsgErro;
+    }
+}
---
+++ Topicos Especiais II/CorretagemDeImoveis/Modelo/Usuario.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Modelo
+{
+    public class Usuario
+    {
+        private String user;
+        private String password;
+        
+
+
+
+        public string User { get => user; set => user = value; }
+        public string Password { get => password; set => password = value; }
+       
+    }
+}
